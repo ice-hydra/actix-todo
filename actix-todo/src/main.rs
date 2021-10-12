a8704fae7d661148df8fed4f28e60141b3f231f1
ice-hydra <icehydra666@gmail.com>
2021-10-12 16:11:22 +0800

    创建第一个应用，并序列化数据到JSON

--- actix-todo/src/main.rs
+++ actix-todo/src/main.rs
@@ -1,10 +1,12 @@
 mod models;
 use actix_web::{web, App, HttpServer, Responder};
 async fn status() -> impl Responder {
-    web::HttpResponse::Ok().json(models::Status { status: "OK" })
+    web::HttpResponse::Ok().json(models::Status { status: "UP" })
 }
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    println!("Starting server at http://127.0.0.1:8080");
+
     HttpServer::new(|| App::new().route("/", web::get().to(status)))
         .bind("0.0.0.0:8080")?
         .run()
