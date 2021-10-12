67e53afd6d6a87b2fd54449899b156567edd9506
ice-hydra <icehydra666@gmail.com>
2021-10-12 16:54:58 +0800

    读取配置

--- actix-todo/src/main.rs
+++ actix-todo/src/main.rs
@@ -1,14 +1,26 @@
+mod config;
 mod models;
+
 use actix_web::{web, App, HttpServer, Responder};
+use dotenv::dotenv;
+
 async fn status() -> impl Responder {
     web::HttpResponse::Ok().json(models::Status { status: "UP" })
 }
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    println!("Starting server at http://127.0.0.1:8080");
+    dotenv().ok();
+
+    let config = config::Config::from_env().unwrap();
+
+    println!(
+        "Starting server at http://{}:{}",
+        config.server.host, config.server.port
+    );
 
     HttpServer::new(|| App::new().route("/", web::get().to(status)))
-        .bind("0.0.0.0:8080")?
+        .bind(format!("{}:{}", config.server.host, config.server.port))?
         .run()
         .await
 }
