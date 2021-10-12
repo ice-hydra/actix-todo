c1aefe02dee9c00adb904ca6a9dce1458cb3190d
ice-hydra <icehydra666@gmail.com>
2021-10-12 17:27:46 +0800

    数据模型

--- actix-todo/src/models.rs
+++ actix-todo/src/models.rs
@@ -1,5 +1,23 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use tokio_pg_mapper_derive::PostgresMapper;
+
 #[derive(Serialize)]
 pub struct Status<'a> {
     pub status: &'a str,
+}
+
+#[derive(Deserialize, PostgresMapper, Serialize)]
+#[pg_mapper(table = "todo_list")]
+pub struct TodoList {
+    pub id: i32,
+    pub title: String,
+}
+
+#[derive(Deserialize, Serialize, PostgresMapper)]
+#[pg_mapper(table = "todo_item")]
+pub struct TodoItem {
+    pub id: i32,
+    pub title: String,
+    pub checked: bool,
+    pub list_id: i32,
 }
