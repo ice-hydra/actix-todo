5eb382372ec29549a5eb15204015baeddb20d112
ice-hydra <icehydra666@gmail.com>
2021-10-12 17:23:37 +0800

    数据配置和连接

--- actix-todo/src/main.rs
+++ actix-todo/src/main.rs
@@ -3,6 +3,7 @@ mod models;
 
 use actix_web::{web, App, HttpServer, Responder};
 use dotenv::dotenv;
+use tokio_postgres::NoTls;
 
 async fn status() -> impl Responder {
     web::HttpResponse::Ok().json(models::Status { status: "UP" })
@@ -14,13 +15,19 @@ async fn main() -> std::io::Result<()> {
 
     let config = config::Config::from_env().unwrap();
 
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
     println!(
         "Starting server at http://{}:{}",
         config.server.host, config.server.port
     );
 
-    HttpServer::new(|| App::new().route("/", web::get().to(status)))
-        .bind(format!("{}:{}", config.server.host, config.server.port))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .route("/", web::get().to(status))
+    })
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
+    .run()
+    .await
 }
